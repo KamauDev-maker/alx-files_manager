855bc712668a6dff57f6f1f7c04f959b6f1645a3
KamauDev-maker <oscarnjenga@gmail.com>
2023-12-05 13:02:48 -0500

    Authenticate a user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import { ObjectId } from 'mongodb';
+
 const { SHA1 } = require('crypto-js');
 const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
 
 class UsersController {
   // eslint-disable-next-line consistent-return
@@ -38,6 +41,25 @@ class UsersController {
     } catch (error) {
       console.error(`Error creating user: ${error}`);
       res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  static async getMe(request, response) {
+    try {
+      const userToken = request.header('X-Token');
+      const authKey = `auth_${userToken}`;
+
+      const userID = await redisClient.get(authKey);
+      console.log('USER KEY GET ME', userID);
+      if (!userID) {
+        response.status(401).json({ error: 'Unauthorized' });
+      }
+      const user = await dbClient.getUser({ _id: ObjectId(userID) });
+
+      response.json({ id: user._id, email: user.email });
+    } catch (error) {
+      console.log(error);
+      response.status(500).json({ error: 'Server error' });
     }
   }
 }
